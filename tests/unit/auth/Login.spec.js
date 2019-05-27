c3106cf13d24bd69f5278e0931af4070aa4a2514
vahdet <vahdetkeskin@gmail.com>
2019-05-27 21:49:39 +0300

    add dummy tests

---
+++ tests/unit/auth/Login.spec.js
@@ -0,0 +1,8 @@
+import { shallowMount } from '@vue/test-utils'
+import Login from '@/components/auth/Login.vue'
+
+describe('Login.vue', () => {
+  it('does nothing', () => {
+    expect(1).toEqual(1)
+  })
+})
