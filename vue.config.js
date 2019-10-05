dcaac318cb21fa066d44dd96287a1358b7afcda7
vahdet <vahdetkeskin@gmail.com>
2019-10-05 23:21:46 +0300

    npm update and edge support for svg-loader

--- vue.config.js
+++ vue.config.js
@@ -30,6 +30,9 @@ module.exports = {
     const svgRule = config.module.rule('svg')
     svgRule.uses.clear()
     svgRule
+      .use('babel-loader')
+      .loader('babel-loader')
+      .end()
       .use('vue-svg-loader')
       .loader('vue-svg-loader')
   }
