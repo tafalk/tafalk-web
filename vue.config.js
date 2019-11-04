0bca75bc74e72514f2fd66fab5a48be6604a96d9
vahdet <vahdetkeskin@gmail.com>
2019-11-04 22:00:13 +0300

    some dependency upgrades

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,8 @@
-// const path = require('path')
-// const PrerenderSPAPlugin = require('prerender-spa-plugin')
-
 module.exports = {
   transpileDependencies: ['vuetify'],
   configureWebpack: {
     // For development purposes
     // devtool: 'source-map',
-    // Prerender some routes for SEO friendliness in a SPA
-    plugins: [
-      /*
-      new PrerenderSPAPlugin({
-        staticDir: path.join(__dirname, 'dist'),
-        // Required - Routes to render.
-        routes: [ '/', '/home', '/content', '/about' ]
-      })
-      */
-    ]
   },
   pwa: {
     themeColor: '#FFB300'
@@ -25,7 +12,7 @@ module.exports = {
       locale: 'en',
       fallbackLocale: 'en',
       localeDir: 'locales',
-      enableInSFC: true
+      enableInSFC: false
     },
     s3Deploy: {
       registry: undefined,
@@ -46,15 +33,15 @@ module.exports = {
       pluginVersion: '3.0.0'
     }
   },
-  chainWebpack: (config) => {
+  chainWebpack: config => {
     // svg loader
-    const svgRule = config.module.rule('svg')
-    svgRule.uses.clear()
+    const svgRule = config.module.rule('svg');
+    svgRule.uses.clear();
     svgRule
       .use('babel-loader')
       .loader('babel-loader')
       .end()
       .use('vue-svg-loader')
-      .loader('vue-svg-loader')
+      .loader('vue-svg-loader');
   }
-}
+};
